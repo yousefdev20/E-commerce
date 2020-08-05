d80ab290a9f2c99a4667ecca4b11de38c4ae0341
yousef1010 <yousefabuanzah2@gmail.com>
2020-08-05 13:14:45 +0300

    test1

--- database/migrations/2020_08_05_101204_create_ddata_types_table.php
+++ database/migrations/2020_08_05_101204_create_ddata_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateDataTypesTable extends Migration {
+class CreateDDataTypesTable extends Migration {
 
 	/**
 	 * Run the migrations.
