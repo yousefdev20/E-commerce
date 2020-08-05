ce3e8c7ab78a53449b14f345b9515645832e1b4b
yousef1010 <yousefabuanzah2@gmail.com>
2020-08-05 13:08:15 +0300

    asdsafasasf

--- database/migrations/2020_08_05_095706_create_users_table.php
+++ database/migrations/2020_08_05_095706_create_users_table.php
@@ -2,43 +2,35 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->bigInteger('id', true)->unsigned();
-			$table->bigInteger('role_id')->unsigned()->nullable()->index('users_role_id_foreign');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('avatar');
-			$table->dateTime('email_verified_at')->nullable();
-			$table->string('password');
-			$table->string('remember_token', 100)->nullable();
-			$table->text('settings', 65535)->nullable();
-			$table->timestamps();
-			$table->string('phone')->nullable();
-			$table->string('longitude')->nullable();
-			$table->string('latitude')->nullable();
-		});
-	}
-
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 }
