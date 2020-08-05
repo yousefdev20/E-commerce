22dffe10aa5a045d26f86b446140f2b2a8535860
yousef1010 <yousefabuanzah2@gmail.com>
2020-08-05 22:23:19 +0300

    up4

--- database/migrations/2020_08_05_115730_create_users_table.php
+++ database/migrations/2020_08_05_115730_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::create('users', function(Blueprint $table)
 		{
-			$table->int('id', true)->unsigned();
+			$table->int('id')->unsigned();
 			$table->int('role_id')->unsigned()->nullable();
 			$table->string('name');
 			$table->string('email')->unique();
